6974cbddcb3c1e0f0c0cdeaa8252405114fd617b
arpit92k <arpit92k@gmail.com>
2015-10-15 23:43:18 +0530

    bug fix for check username

--- login/users/serializers.py
+++ login/users/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from users.models import CheckUser
 
 
-class CheckUserSerializer(serializers.Serializer):
+class CheckUserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = CheckUser
-		fields = ('exists',)
